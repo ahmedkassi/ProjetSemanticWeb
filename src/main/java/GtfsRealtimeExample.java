ffed10d911791160ab19529bac442b2ac595f8df
elIdrissiYounes <y.elidrissiyazami@etu.emse.fr>
2017-12-19 18:35:05 +0000

    hak a zebi

--- src/main/java/GtfsRealtimeExample.java
+++ src/main/java/GtfsRealtimeExample.java
@@ -134,15 +134,16 @@ public class GtfsRealtimeExample {
 
 
     public static void main(String[] args) throws Exception {
+    	//jena
         Model model = ModelFactory.createDefaultModel();
         Map<String, String> vehicleIdsByEntityIds = new HashMap<String, String>();
         Map<String, Vehicle> vehiclesById = new ConcurrentHashMap<String, Vehicle>();
-        URL url = new URL(" http://gtfs.ovapi.nl/nl/vehiclePositions.pb");
+        URL url = new URL("http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb");
        Property geo = ResourceFactory.createProperty("http://www.w3.org/2003/01/geo/wgs84_pos#") ;
         Property lat = ResourceFactory.createProperty("http://www.w3.org/2003/01/geo/wgs84_pos#lat") ;
         Property lon = ResourceFactory.createProperty("http://www.w3.org/2003/01/geo/wgs84_pos#long") ;
 
-       // while(true){
+        while(true){
         FeedMessage feed = FeedMessage.parseFrom(url.openStream());
         for (FeedEntity entity : feed.getEntityList()) {
             if (entity.hasVehicle()) {
@@ -156,10 +157,12 @@ public class GtfsRealtimeExample {
                 v.setLon(position.getLongitude());
                 v.setLastUpdate(System.currentTimeMillis());
                Vehicle existing = vehiclesById.get(vehicleId);
-                if (existing == null || existing.getLat() != v.getLat() || existing.getLon() != v.getLon()) {
-
-                    String   vehicleURI ="http://somewhere/vehicle"+vehicleId;
-
+               String   vehicleURI ="http://somewhere/vehicle"+vehicleId;
+               if (existing == null ) {
+               
+                    
+                  
+                   
                     Resource vehiclerdf = model.createResource(vehicleURI)
                             .addProperty(lat,Double.toString(v.getLat()))
                             .addProperty(lon, Double.toString( v.getLon()));
@@ -170,39 +173,51 @@ public class GtfsRealtimeExample {
                     System.out.println(v.getLon());
                     System.out.println(vehiclerdf.toString());
                     System.out.println("-------------------------------------------------------------");
-                }
+               }
+               Resource r=model.getResource(vehicleURI);
+               if(r.getProperty(lat).getDouble() != v.getLat()){
+                   r.removeAll(lon);
+                   r.addProperty(lon, Double.toString(v.getLon()));
+                   }
+                   if(r.getProperty(lon).getDouble() != v.getLon()){
+                   	r.removeAll(lat);
+                   	r.addProperty(lat, Double.toString(v.getLat()));
+                   }// list the statements in the Model
+                 //   StmtIterator iter = model.listStatements();
+
+            // print out the predicate, subject and object of each statement
+                 /*   while (iter.hasNext()) {
+                    	     Statement stmt      = iter.nextStatement();  // get next statement
+                        Resource  subject   = stmt.getSubject();     // get the subject
+                        Property  predicate = stmt.getPredicate();   // get the predicate
+                        RDFNode   object    = stmt.getObject();      // get the object
+
+                        System.out.print(subject.toString());
+                        System.out.print(" " + predicate.toString() + " ");
+                        if (object instanceof Resource) {
+                            System.out.print(object.toString());
+                        } else {
+                            // object is a literal
+                            System.out.print(" \"" + object.toString() + "\"");
+                        }
+
+                        System.out.println(" .");
+                    }*/
+                model.write(System.out);
+                
+                
             }
         }
-          /*  Thread t = new Thread() {
+            Thread t = new Thread() {
                 public void run() {
                     System.out.println("waiting 30 sec..............");
                 }
             };
             t.start();
-            Thread.sleep(30000);*/
+            Thread.sleep(30000);
 
-     //   }
-// list the statements in the Model
-        StmtIterator iter = model.listStatements();
+       }
 
-// print out the predicate, subject and object of each statement
-        while (iter.hasNext()) {
-            Statement stmt      = iter.nextStatement();  // get next statement
-            Resource  subject   = stmt.getSubject();     // get the subject
-            Property  predicate = stmt.getPredicate();   // get the predicate
-            RDFNode   object    = stmt.getObject();      // get the object
-
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
-            }
-
-            System.out.println(" .");
-        }
 
 
 
